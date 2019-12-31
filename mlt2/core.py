0c0aa932a16ddbb7a3a5ef8c31fbfd15d142629d
ml at t430 <ml at t430@matthiaslesch.de>
2019-12-31 20:33:25 +0100

    Squashed commit of the following:
    commit 3ea0ce70d771f0023eea024e51eb67ba6cf0de0f
    Author: ml at t430 <ml at t430@matthiaslesch.de>
    Date:   Tue Dec 31 20:30:48 2019 +0100
    
        with cli it now works as mltmin2020
    
    commit bd7dcd2f05f8bb8745e35e1915748ae07987a870
    Author: ml at t430 <ml at t430@matthiaslesch.de>
    Date:   Tue Dec 31 19:33:05 2019 +0100
    
        MV mltmin2020.py -> core.py
    
    commit 492aeb7ba8a86c5772d1b479a67ee5fd2af2f73e
    Author: ml at t430 <ml at t430@matthiaslesch.de>
    Date:   Tue Dec 31 19:27:01 2019 +0100
    
        Exception handling myexec

--- mlt2/core.py
+++ mlt2/core.py
@@ -1,31 +1,14 @@
-#!/usr/bin/python3
-# vim: fileencoding=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4
-# vim: expandtab softtabstop=0 nosmarttab guifont=Monospace\ 14 foldmethod=manual noautoindent nocin nosi indentexpr=
+# mlt2/core.py
+# Copyright 2019 Matthias Lesch <ml@matthiaslesch.de>
+# MIT License: http://www.opensource.org/licenses/mit-license.php
+"""
+    mlt2.core
+    ---------
 
-# 2020 reimplementation of minimal mlt
-""" Language
-<? ... ?> embedded python
-<# comment #>
-<! expression !> expression
-or <! expression ! format string >
-so far format string may only be a number. output will be formatted in
-this length rechtsbuendig.
+    The core functionality.
 
-## new feature
-## before processing any tag, it is interpolated according to
-## #{var} a la ruby or $ a la bash/perl.
-## to choose the method, set the variable __ML_subst to
-## either '',$ or #{}
-
-<$ $> auxiliary user Tag.
-
-??
-#Inside python block one can access the environment under env.
-#This is the only variable which is provided.
-
-# special variables in environment:
-__clean__, __keepcode__
-??
+    :copyright: 2014-2020 by Matthias Lesch <ml@matthiaslesch.de>
+    :license:   MIT license
 """
 import os,re,sys
 
@@ -279,74 +262,6 @@ Vorher1+1=<!a+1!>Wei
 User:<$FUCK$>
 <!hello('world')   !>."""
 
-doc = """
-The mltmin2020 python template language
-=======================================
 
-<?
-__ML_subst='$'
-pyopen='<'+'?'
-pyclose='?'+'>'
-evalopen='<'+'!'
-evalclose='!'+'>'
-commentopen='<'+'#'
-commentclose='#'+'>'
-?>
-
-The t in mlt stands for "template", it goes back
-to an old hack. The language embeds python into normal text,
-and is very versatile. The interpreter is written in python and
-has an extremely small footprint, just a few kb of code.
-
-Language description:
-
-1. Normal text is just rendered unaltered
-
-2. Everything inside $pyopen ... $pyclose is run as python code,
-within a fixed environment env. The environment corresponds to 
-the globals inside the python code.
-
-3. Everything inside $evalopen...$evalclose is evaluated as python
-code, also within env. Variables declared in previous python blocks
-are visible.
-
-4. Everything inside $commentopen ... $commentclose is a comment.
-<# obsolete: $commentopen must be at the beginning of a line #>
-
-5. Auxiliary user tag: <Dollar ... Dollar>. By default it is
-a comment.
- 
-The Blocks described in 1.-5. correspond to tokens of the language.
-Before such a token is rendered, it is preprocessed as follows.
-Depending on the value of the variable __ML_subst it is either
-  
-     '': rendered as is
-     '$': Expressions $ followed by a python name must refer to
-          a valid variable. They are substituted by the value of this var
-     '#{}': Same but ruby type #{var} ...
-
-Special variables:
-
-    env['__ML_exec'] = myexec
-    env['__ML_eval'] = myeval
-    env['__ML_comment'] = nothing
-    env['__ML_aux'] = nothing
-    env['__ML_txt'] = noop
-    env['__ML_symboltable'] = symboltable
-
-These variable refer to the processing functions. They my be altered
-inside the python blocks.
-"""    
-
-
-if __name__=='__main__':
-    #fix: templates can now import pymods from their directory
-    sys.path.insert(0,os.path.realpath('.'))
-    print(repr(sys.path))
-    if len( sys.argv )<=1:
-       #print(__file__)
-       #os.system("less /usr/local/bin/mltmin")
-       print( mltminimal( doc, {} ) )
-       sys.exit()
-    with open( sys.argv[1],'r') as f:
-        print( mltminimal( f.read(), {} ) )    
+# -*- coding: utf-8 -*-
+## vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab autoindent tw=79 ft=python fenc=utf-8
